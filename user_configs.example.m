23a3fb24ab631da2fd4afc7354307713d9060fc0
pulkitag <pulkitag@eecs.berkeley.edu>
2014-08-26 23:14:03 -0700

    Added user_configs example

---
+++ user_configs.example.m
@@ -0,0 +1,8 @@
+function [cnf] = user_configs()
+
+	%The directory to store all the data. 
+	cnf.dataDir = '/home/eecs/pulkitag/Research/Documentation/matlab/';
+
+	%The style file for generating the tex-document
+	cnf.stylePkg = 'nips14submit_e';
+end
